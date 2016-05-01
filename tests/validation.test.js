0b9d2ce11985f262947b0b0d4e6f3756e7de114d
Camilo Reyes <reyesc@gmail.com>
2016-05-01 14:04:48 -0500

    Test validations on schema

---
+++ tests/validation.test.js
@@ -0,0 +1,36 @@
+﻿var target = require('../graphql/schema');
+var graphql = require('graphql');
+var should = require('should');
+
+describe('A human type', function () {
+  it('fails without arguments', function (done) {
+    const query = `
+      query HumanWithoutArgument {
+        human {
+        }
+      }
+    `;
+
+    graphql.graphql(target, query).then(function (result) {
+      should.exist(result.errors);
+
+      done();
+    });
+  });
+
+  it('fails with wrong argument type', function (done) {
+    const query = `
+      query HumanWithWrongArgumentType {
+        human(id: "1") {
+          name
+        }
+      }
+    `;
+
+    graphql.graphql(target, query).then(function (result) {
+      should.exist(result.errors);
+
+      done();
+    });
+  });
+});
