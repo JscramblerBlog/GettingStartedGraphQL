2ab530b51d19e59b11eb205ba2424121a3139825
Camilo Reyes <reyesc@gmail.com>
2016-05-02 16:39:00 -0500

    Standardize HTTP headers

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ var url = require('url');
 var schema = require('./graphql/schema');
 var graphql = require('graphql');
 
+var headers = {
+  'Content-Type': 'application/json',
+  'Cache-Control': 'no-cache'
+};
+
 var app = http.createServer(function (req, res) {
   const query = url.parse(req.url, true).query.query;
 
   graphql.graphql(schema, query).then(function (queryResult) {
     if (queryResult.errors) {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Cache-Control': 'no-cache'
-      });
+      res.writeHead(400, headers);
 
-      res.end(queryResult.errors[0].message);
+      res.end(JSON.stringify({ error: queryResult.errors[0].message }));
     } else {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache'
-      });
+      res.writeHead(200, headers);
 
       res.end(JSON.stringify(queryResult));
     }
